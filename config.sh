545ac60fbd9d2828d126d35313638e307dd3e794
Wiktor Niesiobędzki <github@vink.pl>
2018-05-14 22:43:38 +0200

    Sanity restored

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e -x
 
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
@@ -8,15 +7,13 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
 
-    # yum install -y boost148-python boost148-devel sparsehash-devel bzip2-devel zlib-devel
-    yum reinstall -y boost148-python boost148-devel sparsehash-devel bzip2-devel zlib-devel
+    # this is run twice
+
+    yum install -y boost148-python boost148-devel sparsehash-devel bzip2-devel zlib-devel
 
     export BOOST_VERSION=148
     export LIBOSMIUM_PREFIX=/io/libosmium
     export PROTOZERO_PREFIX=/io/protozero
-
-
-    :
 }
 
 function run_tests {
