e63a6cdcaf45b112b137d726d673d17043e84242
Wiktor Niesiobędzki <github@vink.pl>
2018-05-14 20:17:44 +0200

    Initial multibuild configuration

---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e -x
+
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+
+    yum install -y boost148-python boost148-devel sparsehash-devel bzip2-devel zlib-devel
+
+    export BOOST_VERSION=148
+    export LIBOSMIUM_PREFIX=/io/libosmium
+
+    # fix problems with linker
+    ln -s /usr/lib64/libboost_python.so.1.48.0 /usr/lib64/libboost_python.so || ln -s /usr/lib/libboost_python.so.1.48.0 /usr/lib/libboost_python.so
+
+    :
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python pyosmium/tests/run_tests.py
+    python -c "import osmium"
+}
---
+++ env_vars.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export BOOST_VERSION=148
+export LIBOSMIUM_PREFIX=/io/libosmium
