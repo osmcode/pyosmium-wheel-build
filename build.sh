a918763c5ba434ec76a36b22711704538083cc30
Wiktor Niesiobędzki <github@vink.pl>
2018-05-20 08:53:15 +0200

    Set TRAVIS_OS_NAME

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 mkdir -p dist
+
+if [ $(uname) = "Darwin" ] ; then
+    TRAVIS_OS_NAME="osx"
+else
+    TRAVIS_OS_NAME="linux"
+fi
+
 for p in "2.7" "3.4" "3.5" "3.6" ; do
     export MB_PYTHON_VERSION="${p}"
 	source multibuild/common_utils.sh
