4c11fde403fb809e311bc14651cc7702052037e0
Wiktor Niesiobędzki <github@vink.pl>
2018-11-03 17:43:40 +0100

    Pybind works

--- config.sh
+++ config.sh
@@ -3,19 +3,39 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
+CMAKE_BIN_URL=https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.sh
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
 
-
+    RETURN_PWD="$(pwd)"
     if [ -n "$IS_OSX" ] ; then
         brew update
         brew install google-sparsehash || true
     else
         yum install -y sparsehash-devel bzip2-devel zlib-devel
     fi
+
+    ####
+    # CMake
+    ####
+    curl -o /tmp/cmake.sh https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.sh
+    (
+        cd /
+        echo $'y\nn' | bash /tmp/cmake.sh
+    )
+    cmake --version
+    mkdir -p ${RETURN_PWD}/pyosmium/contrib/
+
+    ####
+    # End of CMake
+    ####
+
+    ####
+    # BOOST 
+    ####
     mkdir -p boost
-    RETURN_PWD="$(pwd)"
     cd boost
     export BOOST_PREFIX="$(pwd)"
     curl -L https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2 | tar xfj -
@@ -23,29 +43,8 @@ function pre_build {
     BOOST_ROOT="$(pwd)"
     cd tools/build
     sh bootstrap.sh
-    ./b2 install --prefix="${BOOST_PREFIX}"
+    ./b2 --prefix="${BOOST_PREFIX}" --without-python install 
     cd "${BOOST_ROOT}"
-
-    if [ -n "$IS_OSX" ] ; then
-        cat << EOF > tools/build/src/site-config.jam
-                using clang ;
-                using python : : ${PYTHON_EXE} : $(${PYTHON_EXE} -c 'from sysconfig import get_paths; print(get_paths()["include"])') ;
-EOF
-        echo "Python includes: "
-        ls -d $(${PYTHON_EXE} -c 'from sysconfig import get_paths; print(get_paths()["include"])')
-        ls $(${PYTHON_EXE} -c 'from sysconfig import get_paths; print(get_paths()["include"])')
-    else
-        cat << EOF > tools/build/src/site-config.jam
-                using gcc ;
-                using python : : $(cpython_path "${PYTHON_VERSION}" "${UNICODE_WIDTH}")/bin/python : $(echo $(cpython_path "${PYTHON_VERSION}" "${UNICODE_WIDTH}")/include/*) ;
-EOF
-    fi
-
-    echo "Using following BOOST configuration:"
-    cat tools/build/src/site-config.jam
-
-    echo "Using PYTHON_VERSION: ${PYTHON_VERSION}"
-    "${BOOST_PREFIX}"/bin/b2 --with-python --prefix="${BOOST_PREFIX}" stage install
 
     if [ -n "$IS_OSX" ] ; then
         export DYLD_LIBRARY_PATH="${BOOST_PREFIX}/lib:${DYLD_LIBRARY_PATH}"
@@ -61,11 +60,18 @@ EOF
         ldconfig ${BOOST_PREFIX}/lib
     fi
 
-    cd "${RETURN_PWD}"
+
+    ####
+    # END of BOOST stuff
+    ####
+
+    echo "Using PYTHON_VERSION: ${PYTHON_VERSION}"
     export LIBOSMIUM_PREFIX=${RETURN_PWD}/libosmium
     export PROTOZERO_PREFIX=${RETURN_PWD}/protozero
     echo "Coming back to ${RETURN_PWD}"
+    cd "${RETURN_PWD}"
 
+    export CXX_FLAGS="-D__STDC_FORMAT_MACROS=1"
 }
 
 function run_tests {
