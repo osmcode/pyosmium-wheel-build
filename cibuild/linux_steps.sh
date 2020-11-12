6872e152a589fe3f495430daff22bbd6ae1ff816
Wiktor Niesiobędzki <github@vink.pl>
2020-11-12 21:22:37 +0100

    Try to test outside cibuildwheel

--- cibuild/linux_steps.sh
+++ cibuild/linux_steps.sh
@@ -52,7 +52,7 @@ ln -sf $(pwd)/libosmium pyosmium/contrib/libosmium
 ln -sf $(pwd)/protozero pyosmium/contrib/protozero
 
 
-yum install -y sparsehash-devel expat-devel boost-devel zlib-devel geos
+yum install -y sparsehash-devel expat-devel boost-devel zlib-devel
 
 
 # install bzip2
